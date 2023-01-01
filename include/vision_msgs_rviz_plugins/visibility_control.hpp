97876267efe4830e6099a7e67a7959085b672080
Georg Novotny <georg.novotny@aon.at>
2023-01-01 18:25:04 +0100

    Fixed visibility of symbols in shared library

--- include/vision_msgs_rviz_plugins/visibility_control.hpp
+++ include/vision_msgs_rviz_plugins/visibility_control.hpp
@@ -1,35 +1,61 @@
-#ifndef DETECTION_3D_ARRAY_DISPLAY_VISIBILITY_CONTROL_HPP
-#define DETECTION_3D_ARRAY_DISPLAY_VISIBILITY_CONTROL_HPP
+#ifndef VISION_MSGS_RVIZ_PLUGINS_VISIBILITY_CONTROL_HPP
+#define VISION_MSGS_RVIZ_PLUGINS_VISIBILITY_CONTROL_HPP
 
 // This logic was borrowed (then namespaced) from the examples on the gcc wiki:
 //     https://gcc.gnu.org/wiki/Visibility
 
 #if defined _WIN32 || defined __CYGWIN__
     #ifdef __GNUC__
-        #define Detection3DArrayDisplay_EXPORT __attribute__((dllexport))
-        #define Detection3DArrayDisplay_IMPORT __attribute__((dllimport))
+        #define DETECTION_3D_DISPLAY_HPP_EXPORT __attribute__((dllexport))
+        #define DETECTION_3D_DISPLAY_HPP_IMPORT __attribute__((dllimport))
+
+        #define DETECTION_3D_ARRAY_DISPLAY_HPP_EXPORT __attribute__((dllexport))
+        #define DETECTION_3D_ARRAY_DISPLAY_HPP_IMPORT __attribute__((dllimport))
     #else
-        #define Detection3DArrayDisplay_EXPORT __declspec(dllexport)
-        #define Detection3DArrayDisplay_IMPORT __declspec(dllimport)
+        #define DETECTION_3D_DISPLAY_HPP_EXPORT __declspec(dllexport)
+        #define DETECTION_3D_DISPLAY_HPP_IMPORT __declspec(dllimport)
+
+        #define DETECTION_3D_ARRAY_DISPLAY_HPP_EXPORT __declspec(dllexport)
+        #define DETECTION_3D_ARRAY_DISPLAY_HPP_IMPORT __declspec(dllimport)
     #endif
-    #ifdef Detection3DArrayDisplay_BUILDING_LIBRARY
-        #define Detection3DArrayDisplay_PUBLIC Detection3DArrayDisplay_EXPORT
+
+    #ifdef DETECTION_3D_DISPLAY_HPP_BUILDING_LIBRARY
+        #define DETECTION_3D_DISPLAY_HPP_PUBLIC DETECTION_3D_DISPLAY_HPP_EXPORT
     #else
-        #define Detection3DArrayDisplay_PUBLIC Detection3DArrayDisplay_IMPORT
+        #define DETECTION_3D_DISPLAY_HPP_PUBLIC DETECTION_3D_DISPLAY_HPP_IMPORT
     #endif
-    #define Detection3DArrayDisplay_PUBLIC_TYPE Detection3DArrayDisplay_PUBLIC
-    #define Detection3DArrayDisplay_LOCAL
+    #define DETECTION_3D_DISPLAY_HPP_PUBLIC_TYPE DETECTION_3D_DISPLAY_HPP_PUBLIC
+    #define DETECTION_3D_DISPLAY_HPP_LOCAL
+
+    #ifdef DETECTION_3D_ARRAY_DISPLAY_HPP_BUILDING_LIBRARY
+    #define DETECTION_3D_ARRAY_DISPLAY_HPP_PUBLIC DETECTION_3D_ARRAY_DISPLAY_HPP_EXPORT
+    #else
+        #define DETECTION_3D_ARRAY_DISPLAY_HPP_PUBLIC DETECTION_3D_ARRAY_DISPLAY_HPP_IMPORT
+    #endif
+    #define DETECTION_3D_ARRAY_DISPLAY_HPP_PUBLIC_TYPE DETECTION_3D_ARRAY_DISPLAY_HPP_PUBLIC
+    #define DETECTION_3D_ARRAY_DISPLAY_HPP_LOCAL
+
 #else
-    #define Detection3DArrayDisplay_EXPORT __attribute__((visibility("default")))
-    #define Detection3DArrayDisplay_IMPORT
+    #define DETECTION_3D_DISPLAY_HPP_EXPORT __attribute__((visibility("default")))
+    #define DETECTION_3D_DISPLAY_HPP_IMPORT
+
+    #define DETECTION_3D_ARRAY_DISPLAY_HPP_EXPORT __attribute__((visibility("default")))
+    #define DETECTION_3D_ARRAY_DISPLAY_HPP_IMPORT
     #if __GNUC__ >= 4
-        #define Detection3DArrayDisplay_PUBLIC __attribute__((visibility("default")))
-        #define Detection3DArrayDisplay_LOCAL __attribute__((visibility("hidden")))
+        #define DETECTION_3D_DISPLAY_HPP_PUBLIC __attribute__((visibility("default")))
+        #define DETECTION_3D_DISPLAY_HPP_LOCAL __attribute__((visibility("hidden")))
+
+        #define DETECTION_3D_ARRAY_DISPLAY_HPP_PUBLIC __attribute__((visibility("default")))
+        #define DETECTION_3D_ARRAY_DISPLAY_HPP_LOCAL __attribute__((visibility("hidden")))
     #else
-        #define Detection3DArrayDisplay_PUBLIC
-        #define Detection3DArrayDisplay_LOCAL
+        #define DETECTION_3D_DISPLAY_HPP_PUBLIC
+        #define DETECTION_3D_DISPLAY_HPP_LOCAL
+
+        #define DETECTION_3D_ARRAY_DISPLAY_HPP_PUBLIC
+        #define DETECTION_3D_ARRAY_DISPLAY_HPP_LOCAL
     #endif
-    #define Detection3DArrayDisplay_PUBLIC_TYPE
+    #define DETECTION_3D_DISPLAY_HPP_PUBLIC_TYPE
+    #define DETECTION_3D_ARRAY_DISPLAY_HPP_PUBLIC_TYPE
 #endif
 
-#endif // DETECTION_3D_ARRAY_DISPLAY_VISIBILITY_CONTROL_HPP
+#endif // VISION_MSGS_RVIZ_PLUGINS_VISIBILITY_CONTROL_HPP
